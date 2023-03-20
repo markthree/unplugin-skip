285b6eb51628dcca81a024d4aeedb3e703d08afb
markthree <1801982702@qq.com>
2023-03-20 18:10:59 +0800

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import { resolve } from 'path'
-const foo = 1
+import { createUnplugin } from 'unplugin'
 
-export { resolve, foo }
+export const unplugin = createUnplugin(options => {
+	return {
+		name: 'unplugin-skip',
+		transformInclude(id) {
+			return id.endsWith('.vue')
+		},
+		transform(code, id) {
+			console.log(id)
+			return code.replace(
+				/<template>/,
+				'<template><div>Injected</div>'
+			)
+		}
+	}
+})
+
+export const vitePlugin = unplugin.vite
+export const rollupPlugin = unplugin.rollup
+export const webpackPlugin = unplugin.webpack
+export const rspackPlugin = unplugin.rspack
+export const esbuildPlugin = unplugin.esbuild
